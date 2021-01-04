202b512e36ac72998430b3bcdedb6d11f08a9a8b
AtSixPm <tjdrms4206@naver.com>
2021-01-05 00:26:41 +0900

    Create Mongodb Database Table & Node.js Databese Connection setting

--- server.js
+++ server.js
@@ -1,38 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { json } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test'); // 기본 설정에 따라 포트가 상이 할 수 있습니다.
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("mongo db connection OK.");
+});
+
+
+// customer 목록 가져오는 api
 app.get('/api/customers', (req, res)=>{
-    res.send([
-        {
-            'id' : 1,
-            'image' : 'https://placeimg.com/64/64/1',
-            'name' : '나동빈',
-            'birthday' : '961222',
-            'gender' : '남자',
-            'job' : '대학생'
-          },
-          {
-            'id' : 2,
-            'image' : 'https://placeimg.com/64/64/2',
-            'name' : '홍길동',
-            'birthday' : '960305',
-            'gender' : '남자',
-            'job' : '프로그래머'
-          },
-          {
-            'id' : 3,
-            'image' : 'https://placeimg.com/64/64/3',
-            'name' : '이순신',
-            'birthday' : '910203',
-            'gender' : '남자',
-            'job' : '장군'
-          }
-    ]);
+  Customer.find({})
+  .then(result=> (
+    res.send(
+      result ))
+      ).catch(err=> console.log);
+    
 })
 
+
+
+/* 스키마 */
+const customersSchema = new mongoose.Schema({
+  id: String,
+  image: String,
+  name: String,
+  birthday: String,
+  gender: String,
+  job: String
+});
+const Customer = mongoose.model('customer', customersSchema);
+
+
 app.listen(port, () => console.log('Listening on port ${port}'));
+
+
